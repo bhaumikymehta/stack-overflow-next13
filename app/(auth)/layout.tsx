d6cbeac78856ec307dce6139930818aeacaa8606
bhaumikmehta <bhaumikymehta@gmail.com>
2024-02-14 22:26:16 -0600

    Add new migrations and update configuration files

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
       {children}
     </main>
-  );
+  )
 }
+
+export default Layout
